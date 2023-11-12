cb1e26bc455f42b02f906a3d57ac44e2d4cfcfe2
Oxt4n1nc9b4r <150040423+Oxt4n1nc9b4r@users.noreply.github.com>
2023-11-12 15:29:56 +0600

    Snake.py

---
+++ Snake.py
@@ -0,0 +1,62 @@
+# Author T4n1n
+
+import random
+import curses
+
+stdscr = curses.initscr()
+curses.curs_set(0)
+sh, sw = stdscr.getmaxyx()
+w = curses.newwin(sh, sw, 0, 0)
+w.keypad(1)
+w.timeout(100)
+
+snake = [
+    [sh // 2, sw // 2],
+    [sh // 2, sw // 2 - 1],
+    [sh // 2, sw // 2 - 2]
+]
+
+food = [sh // 2, sw // 2 + 5]
+w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+
+key = curses.KEY_RIGHT
+
+while True:
+    next_key = w.getch()
+    key = key if next_key == -1 else next_key
+
+    new_head = [snake[0][0], snake[0][1]]
+
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
+
+    snake.insert(0, new_head)
+
+    if snake[0][0] == 0 or snake[0][0] == sh or snake[0][1] == 0 or snake[0][1] == sw:
+        curses.endwin()
+        quit()
+
+    if snake[0] in snake[1:]:
+        curses.endwin()
+        quit()
+
+    if snake[0] == food:
+        food = None
+        while food is None:
+            nf = [
+                random.randint(1, sh - 1),
+                random.randint(1, sw - 1)
+            ]
+            food = nf if nf not in snake else None
+        w.addch(food[0], food[1], curses.ACS_PI)
+    else:
+        tail = snake.pop()
+        w.addch(int(tail[0]), int(tail[1]), ' ')
+
+    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
